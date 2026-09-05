b6c079f37bf9d9895dd28776bd9f32f17638bc75
sarina-loli <sarina123getu@gmail.com>
2026-09-05 12:05:27 -0700

    2

--- config/settings.py
+++ config/settings.py
@@ -83,16 +83,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_management',
-        'USER': 'root',
-        'PASSWORD': 'loli123,sara',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'))} 
 
 
 # Password validation
